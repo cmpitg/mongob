6a013c74ee6ee27667ee4c849d287e7fc93c17ee
Duong Nguyen <cmpitg@gmail.com>
2015-07-17 16:13:29 +0700

    feat: add task file as Makefile alternative

---
+++ tasks.py
@@ -0,0 +1,37 @@
+import os
+
+from invoke import task, run
+
+
+@task
+def setup():
+    """
+    Prepares to run tests.
+    """
+    os.chdir(os.path.dirname(__file__))
+
+
+@task
+def test_all():
+    """
+    Runs all test sets.
+    """
+    test_01()
+    test_02()
+    test_03()
+
+
+@task(pre=[setup])
+def test_01():
+    """
+    Tests Mongo backup with fresh DB.
+    """
+    print('=== Test set: Fresh backup ===')
+    os.chdir('test/set01_fresh/')
+    run('./setup_test.py')
+    run(
+        '../../src/mongo_backup.py --config config.yaml'
+        + ' --progress-file current_progress.yaml'
+        + ' --log backup.log'
+    )
+    run('./teardown_test.py')
