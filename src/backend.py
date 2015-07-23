b72c9353a2af09ba30b5d1e91973313ab1a81ec3
Duong Nguyen <cmpitg@gmail.com>
2015-07-23 10:06:28 +0700

    feat: re-implement get_db in module backend

--- src/backend.py
+++ src/backend.py
@@ -6,10 +6,34 @@ from io import TextIOBase
 
 @dispatch(TextIOBase)
 def close(f):
+##############################################################################
+
+def get_db(uri, connections):
     """
-    Closes stream.
+    Retrieves database from connection string.  The connection string
+    follows one of the formats:
+    'mongodb://[username][[:[password]]@]<host>/<db_name>'
+    or 'file://<path>'
+    or simply '<path>'.
     """
-    f.close()
+
+    if uri.startswith('mongodb://'):
+        db_name_pos = uri.rfind("/") + 1
+        client      = MongoClient(uri[:db_name_pos])
+        db          = client[uri[db_name_pos:]]
+    else:
+        if uri.startswith("file://"):
+            uri = uri[len("file://"):]
+        client      = uri
+        db          = uri
+
+    try:
+        connections.index(client)
+    except Exception:
+        connections.append(client)
+
+    return connections, db
+
 
 
 @dispatch(MongoClient)
