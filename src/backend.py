bea5ea8c4bc2b4342adb199d6b7e9012476030a2
Duong Nguyen <cmpitg@gmail.com>
2015-07-23 10:07:18 +0700

    feat: implement multimethod for backup backend

--- src/backend.py
+++ src/backend.py
@@ -1,11 +1,16 @@
+import os
+import gzip
+
 from multipledispatch import dispatch
+from bson.json_util import loads as json_loads
+from bson.json_util import dumps as json_dumps
 
 from pymongo.mongo_client import MongoClient
+from pymongo.database     import Database
+from pymongo.collection   import Collection
 from io import TextIOBase
 
 
-@dispatch(TextIOBase)
-def close(f):
 ##############################################################################
 
 def get_db(uri, connections):
@@ -40,10 +45,53 @@ def get_db(uri, connections):
 # print(get_db("/tmp/hi_hi.txt",                  []))
 
 
+##############################################################################
+
+@dispatch(str)
+def close(path):
+    pass
+
 
 @dispatch(MongoClient)
-def close(f):
+def close(client):
     """
     Closes connection to MongoDB server.
     """
-    f.close()
+    client.close()
+
+
+##############################################################################
+
+@dispatch(str)
+def dest_name(path):
+    """
+    Retrieves name of destination collection.
+    """
+    return os.path.basename(path)
+
+
+@dispatch(Collection)
+def dest_name(coll):
+    """
+    Retrieves name of destination file.
+    """
+    return coll.name
+
+
+##############################################################################
+
+@dispatch(str)
+def dest_size(path):
+    """
+    Retrieves size of backed up data.
+    """
+    with open(path, 'w') as input:
+        return json_loads(input.read())
+
+
+@dispatch(Collection)
+def dest_size(coll):
+    """
+    Retrieves size of backed up data.
+    """
+    return coll.count()
